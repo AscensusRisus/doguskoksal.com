ade18c52db1a0c16163e039ea9f70abf60c7c348
Doğuş Köksal <110297054+AscensusRisus@users.noreply.github.com>
2026-07-30 15:02:33 +0300

    Improve initial page load performance

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,4 +1,4 @@
 export const GET = () => new Response(
-  'User-agent: *\nAllow: /\nSitemap: https://doguskoksal.com/sitemap.xml',
+  'User-agent: *\nAllow: /\nSitemap: https://www.doguskoksal.com/sitemap.xml',
   { headers: { 'Content-Type': 'text/plain; charset=utf-8' } },
 );
